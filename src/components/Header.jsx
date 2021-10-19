34caf53c608f3dd314f916d4947ca81fd7254c77
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2021-10-19 14:32:52 +0300

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import img from './images/img.jpg'
-function Header () {
-    return <img className="backgroundImg" src={img} alt="" />;
-}
-export default Header;
--- src/components/Heading1.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../App'
-import './styles.css'
-let name1;
-function Heading1 (props){
-    return <h1 className="Heading1">{props.name1}</h1>;
-}
-export default Heading1;
