cec404819af35f9229315b42615876ecd5a6b8d3
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-04-01 15:03:11 +0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import './Footer.css'
-import BurgerHouse from './images/BurgerHouse.png'
-function Footer (){
-    return <div className="footerDiv" >
-        <img className="burgerHouse" src={BurgerHouse} alt="" />
-    </div>;
-}
-export default Footer;
