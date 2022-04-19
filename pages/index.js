01828b1c4ad2b633bddb99957a09d44c2081b975
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-04-19 16:27:00 +0300

    Update index.js
    Added title and metadata

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,28 @@
-import EventList from '../Components/events/event-list';
-// import getFeaturedEvents from '/dummy-data'
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'Programming for everyone',
-        description:
-            'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
-        location: 'Somestreet 25, 12345 San Somewhereo',
-        date: '2021-05-12',
-        image: 'images/coding-event.jpg',
-        isFeatured: false,
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import Head from 'next/head'
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name="description"
+          content="Find a lot of Events" />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
     },
-    {
-        id: 'e2',
-        title: 'Networking for introverts',
-        description:
-            "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-        location: 'New Wall Street 5, 98765 New Work',
-        date: '2021-05-30',
-        image: 'images/introvert-event.jpg',
-        isFeatured: true,
-    },
-    {
-        id: 'e3',
-        title: 'Networking for extroverts',
-        description:
-            'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
-        location: 'My Street 12, 10115 Broke City',
-        date: '2022-04-10',
-        image: 'images/extrovert-event.jpg',
-        isFeatured: true,
-    },
-];
-
-function getFeaturedEvents() {
-    return DUMMY_EVENTS.filter((event) => event.isFeatured);
+    revalidate: 1800
+  }
 }
 
-function getAllEvents() {
-    return DUMMY_EVENTS;
-}
-
-function getFilteredEvents(dateFilter) {
-    const { year, month } = dateFilter;
-
-    let filteredEvents = DUMMY_EVENTS.filter((event) => {
-        const eventDate = new Date(event.date);
-        return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
-    });
-
-    return filteredEvents;
-}
-
-function getEventById(id) {
-    return DUMMY_EVENTS.find((event) => event.id === id);
-}
-
-// this is the page that will load if we visit our-domain-name.com/
-function HomePage() {
-    const featuredEvents = getFeaturedEvents();
-    return (
-        <div>
-            <h1>The HomePage</h1>
-            <EventList items={featuredEvents} />
-        </div>
-    );
-}
 export default HomePage;
