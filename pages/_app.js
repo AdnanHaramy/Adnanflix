9545cf44e6da7c6ee8122fefeb2ae8d1f246487b
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-04-21 20:33:08 +0300

    Added general head and title description for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from 'next/head'
+import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <Layout>
+      <Head>
+        {/* A head tag that is added to all the pages */}
+        {/* the title and the description will be overwritten by other pages 
+        but if we forget to set the title or the description for any page we will 
+        have our default title and description that will apply to all pages */}
+        <title>Next Events</title>
+        <meta name="description" content='NextJs Events' />
+        <meta name="viewport" content='initial-scale=1.0 , width=device-width ' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 export default MyApp;
