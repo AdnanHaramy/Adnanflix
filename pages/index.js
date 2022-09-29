eff0a51cfaede6c9b935671febb8b19a9dd11b14
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-09-29 21:32:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,6 @@
-import { getFeaturedEvents } from '../helpers/api-util';
-import EventList from '../components/events/event-list';
-import Head from 'next/head'
-function HomePage(props) {
-  return (
-    <div>
-      <Head>
-        <title>NextJs Events</title>
-        <meta name="description"
-          content="Find a lot of Events" />
-      </Head>
-      <EventList items={props.events} />
+function AllEventsPage() {
+    return <div>
+        <h1>All Events Page</h1>
     </div>
-  );
 }
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents
-    },
-    revalidate: 1800
-  }
-}
-
-export default HomePage;
+export default AllEventsPage;
