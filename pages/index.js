d6551576aade1e1d9c3f6390ec520d9c17afea00
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-09-30 00:30:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-function AllEventsPage() {
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+function HomePage() {
+
+    const featuredEvents = getFeaturedEvents();
+
     return <div>
-        <h1>All Events Page</h1>
+        {/* passing the data to the EventList component and
+     the EventList component will pass it to the EventItem component */}
+        <EventList
+            items={featuredEvents}
+        />
     </div>
 }
-export default AllEventsPage;
+
+export default HomePage;
