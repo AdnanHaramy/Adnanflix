eff6eae66585d0b96eead24c541ca068c32897f8
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-09-30 00:31:52 +0300

    Update index.js

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,7 @@
 // this the page that will load when we visit our-domain-name/events
-function Events() {
-    return (
-        <div>
-            <h1>This is the Events Page</h1>
-        </div>
-    );
+function AllEventsPage() {
+    return <div>
+        <h1>All Events Page</h1>
+    </div>
 }
-export default Events;
+export default AllEventsPage;
