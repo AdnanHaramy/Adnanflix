e1f8913f4c723811308dcb7fd92b3520854f63ac
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-10-03 20:49:19 +0300

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,8 +1,19 @@
 import Link from "next/link";
-
+import classes from './button.module.css'
 function Button(props) {
-    return <Link>
-
-    </Link>
+    // if the props passed to the button contain a link it should navigate between pages
+    if (props.link) {
+        return <Link href={props.link} >
+            <a className={classes.btn} >
+                {props.children}
+            </a>
+        </Link>
+    }
+    // if not it will behave as a regular button and do the functionallity passed into it 
+    else {
+        return <button className={classes.btn} onClick={props.onClick} >
+            {props.children}
+        </button>
+    }
 }
 export default Button;
